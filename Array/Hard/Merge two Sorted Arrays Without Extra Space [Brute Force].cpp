175588c413e150c8ca9a36914379abab69d7345c
ChiragSinghCS <chiragsinghcs17@gmail.com>
2026-06-06 22:33:50 +0530

    06-06-2026

--- Array/Hard/Merge two Sorted Arrays Without Extra Space [Brute Force].cpp
+++ Array/Hard/Merge two Sorted Arrays Without Extra Space [Brute Force].cpp
@@ -6,13 +6,9 @@ using namespace std;
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        m+n;
-        for(int i = 0; i<n; i++){
-            nums1.push_back(nums2[i]);
-        }
-        sort(nums1.begin(),nums1.end());
-        return
+        for(int i = 0; i < n; i++){
+            nums1[m + i] = nums2[i]; 
+        }        
+        sort(nums1.begin(), nums1.end());
     }
 };
-
-int main(){}
