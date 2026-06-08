373f86e9ef378d5a31d84d5f5437ad382bbdcfb7
ChiragSinghCS <chiragsinghcs17@gmail.com>
2026-06-08 16:58:09 +0530

    08-06-2026

---
+++ Array/Hard/Reverse Pairs [Brute Force].cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int reversePairs(vector<int>& nums) {
+        int n = nums.size();
+        int count = 0;
+        for(int i = 0; i<n; i++){
+            for(int j = i+1; j<n; j++){
+                if(nums[i]>2*nums[j]){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
