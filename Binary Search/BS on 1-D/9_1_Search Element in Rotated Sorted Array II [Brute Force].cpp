3deb229ec1dea28e8e5ef398b6eb50e083014d42
ChiragSinghCS <chiragsinghcs17@gmail.com>
2026-06-14 07:47:15 +0530

    7:45am

--- Binary Search/BS on 1-D/9_1_Search Element in Rotated Sorted Array II [Brute Force].cpp
+++ Binary Search/BS on 1-D/9_1_Search Element in Rotated Sorted Array II [Brute Force].cpp
@@ -5,6 +5,12 @@ using namespace std;
 class Solution {
 public:
     bool searchInARotatedSortedArrayII(vector<int> &nums, int k)  {
-      
+        int n = nums.size();
+        for(int i = 0; i<n; i++){
+            if(nums[i]==k){
+                return true;
+            }
+        }
+        return false;
     }
 };
