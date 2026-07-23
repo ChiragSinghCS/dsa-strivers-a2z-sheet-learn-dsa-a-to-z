c14264c102f2fa11abc30c7463c4d3ac47b95e4d
ChiragSinghCS <chiragsinghcs17@gmail.com>
2026-07-23 23:35:15 +0530

    23-07-2026

---
+++ 5_Strings/Remove Outermost Parentheses.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        int count = 0;
+        string ans = "";
+        for(int i = 0; i<s.length(); i++){
+            if(s[i]=='('){
+                if(count > 0){
+                    ans +=s[i];
+                }
+                count++;
+            }
+            else if(s[i]==')'){
+                count--;
+                if(count > 0){
+                    ans +=s[i];
+                }
+            }
+        }
+        return ans;
+    }
+};
