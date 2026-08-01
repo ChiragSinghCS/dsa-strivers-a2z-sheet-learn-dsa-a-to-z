6e821a6c37a218d595eff0ddf3380d36708e4e62
ChiragSinghCS <chiragsinghcs17@gmail.com>
2026-08-01 13:05:58 +0530

    01-08-2026

--- 5_Strings/Largest Odd Number in a String.cpp
+++ 5_Strings/Largest Odd Number in a String.cpp
@@ -10,7 +10,12 @@ public:
         for(int i = n-1; i >= 0; i--){
             int digit = s[i]-'0';
             if(digit%2!=0){
-                return s.substr(0,i+1);
+                string ans = s.substr(0,i+1);
+                int start = 0;
+                while(start < ans.length() && ans[start]=='0'){
+                    start++;
+                }
+                return ans.substr(start);
             }
         }
         return "";
