3bb7d852bfbd0226a6c917c3f648f33686dd9dfb
ChiragSinghCS <chiragsinghcs17@gmail.com>
2026-08-07 01:01:06 +0530

    06-08-2026

--- 5_Strings/Isomorphic String.cpp
+++ 5_Strings/Isomorphic String.cpp
@@ -7,6 +7,15 @@ using namespace std;
 class Solution {
 public:
     bool isomorphicString(string s, string t) {
-    	//your code goes here
+    	int n = s.length();
+        int m1[256] = {0}, m2[256]={0};
+        for(int i = 0; i<n; i++){
+            if(m1[s[i]]!=m2[s[i]]){
+                return false;
+            }
+            m1[s[i]] = i+1;
+            m2[t[i]] = i+1;
+        }
+        return true;
     }
 };
