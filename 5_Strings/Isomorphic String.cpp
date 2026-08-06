0c2d62903c8e10f0458a75ca5d5e4e6cb00b592c
ChiragSinghCS <chiragsinghcs17@gmail.com>
2026-08-07 01:05:41 +0530

    06-08-2026

--- 5_Strings/Isomorphic String.cpp
+++ 5_Strings/Isomorphic String.cpp
@@ -10,7 +10,7 @@ public:
     	int n = s.length();
         int m1[256] = {0}, m2[256]={0};
         for(int i = 0; i<n; i++){
-            if(m1[s[i]]!=m2[s[i]]){
+            if(m1[s[i]]!=m2[t[i]]){
                 return false;
             }
             m1[s[i]] = i+1;
